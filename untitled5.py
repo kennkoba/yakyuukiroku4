05d22545be8fc95e0ec368e4fe968f897ac9e781
kennkoba <93519403+kennkoba@users.noreply.github.com>
2021-11-27 23:16:48 +0900

    Update untitled5.py

--- untitled5.py
+++ untitled5.py
@@ -1,7 +1,30 @@
-# -*- coding: utf-8 -*-
+ -*- coding: utf-8 -*-
 """
 Created on Sat Nov 27 22:47:17 2021
 
 @author: takumi
 """
 
+import streamlit as st
+
+import pandas as pd
+
+st.title('セリーグ打者記録')
+DATA_URL=('pandas_sample.csv')
+DATE_COLUMN='datepep'
+
+def load_data(nrows):
+    data=pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase=lambda x: str(x).lower()
+    data.rename(lowercase,axis='columns',)
+    return data
+
+data_load_state=st.text('Loading data...')
+data=load_data(10000)
+data_load_state.text('Loading data...done!')
+
+
+st.dataframe(data.style.highlight_max(axis=0),width=900,height=1000)
+
+erea_list = data['球団'].unique()
+selected_erea=st.sidebar.selectbox('表示する球団を選択:',data)
