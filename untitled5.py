44df8ffce7ef37b25a163c60f3d149d9a77ece82
kennkoba <93519403+kennkoba@users.noreply.github.com>
2021-11-28 22:10:46 +0900

    Update untitled5.py

--- untitled5.py
+++ untitled5.py
@@ -12,6 +12,20 @@ import pandas as pd
 
 
 
-
-
 st.title('セリーグ打者記録')
+DATA_URL=('pandas_sample.csv')
+DATE_COLUMN='datepep'
+
+
+def load_data(nrows):
+    data=pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase=lambda x: str(x).lower()
+    data.rename(lowercase,axis='columns',)
+    return data
+
+data_load_state=st.text('Loading data...')
+data=load_data(10000)
+data_load_state.text('Loading data...done!')
+
+
+st.dataframe(data.style.highlight_max(axis=0),width=900,height=1000)
