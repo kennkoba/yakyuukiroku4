5f7792a64101b8977456eb5caabb771f2a344126
kennkoba <93519403+kennkoba@users.noreply.github.com>
2021-11-28 23:45:37 +0900

    Update untitled5.py

--- untitled5.py
+++ untitled5.py
@@ -31,10 +31,4 @@ erea_list =data['球団'].unique()
     
 selected_erea=st.sidebar.selectbox('表示する球団を選択:',erea_list)
 
-df=data[data['球団']== selected_erea]
-selected_erea = st.multiselect('グラフに表示する球団を選択', erea_list, default='巨人')
-df=data[(data['球団'].isin(selected_erea))]
-st.write(px.bar(df,x='選手名',y='打率',title='打率'))
-st.write(px.bar(df,x='選手名',y='本塁打',title='本塁打'))
-st.write(px.bar(df,x='選手名',y='打点',title='打点'))
-st.write('Sportsnavi プロ野球個人成績参照')
+
